87bc350a9dc60d93a86f7ca8244b3d743209de97
HugoDelval <hugodelval@gmail.com>
2017-07-10 00:32:42 +0100

    Add HubSpot experience

--- website/skills.py
+++ website/skills.py
@@ -2,17 +2,28 @@ skills = [
     {
         "name": "Python",
         "note": 5,
-        "description": "<i>Django</i>, Flask, and a <b>LOT</b> of scripting.<br>Python is the language I use every day whenever I have something to automate."
+        "description": "<i>Django</i>, Flask, and a <b>LOT</b> of scripting.<br>Python is the language I use every "
+                       "day whenever I have something to automate. "
     },
     {
         "name": "Application Security",
         "note": 5,
-        "description": "I love web security. RCE / SQLi / XSS / SSRF / race conditions etc...<br>I do a lot of CTFs (Capture The Flag) and challenges online (<a target='_blank' href='https://root-me.org'>root-me.org</a> for example.).<br>Also my two previous intership were in pentesting."
+        "description": "I love web security. RCE / SQLi / XSS / SSRF / race conditions etc...<br>I do a lot of CTFs ("
+                       "Capture The Flag) and challenges online (<a target='_blank' "
+                       "href='https://root-me.org'>root-me.org</a> for example.).<br>Also my two previous internship "
+                       "were in pentesting. "
     },
     {
         "name": "Linux",
         "note": 5,
-        "description": "Really vague isn't it ? I love Linux, especially Debian based distributions. That's the OS I use every day. I created CTFs based on this OS (an example here: <a target='_blank' href='https://github.com/HugoDelval/pedagogic_ctf'>Pedagogic CTF</a>)."
+        "description": "Really vague isn't it ? I love Linux, especially Debian based distributions. That's the OS I "
+                       "use every day. I created CTFs based on this OS (an example here: <a target='_blank' "
+                       "href='https://github.com/HugoDelval/pedagogic_ctf'>Pedagogic CTF</a>). "
+    },
+    {
+        "name": "JAVA",
+        "note": 5,
+        "description": "Some experiences in both Java and Android. At HubSpot all the work was done in Java"
     },
     {
         "name": "French",
@@ -27,22 +38,30 @@ skills = [
     {
         "name": "Docker",
         "note": 4,
-        "description": "Really usefull software containers."
+        "description": "Really useful, I've used it to isolate services when hosting a CTF, the deployment of a "
+                       "dockerised application is really easy. "
+    },
+    {
+        "name": "Kafka",
+        "note": 4,
+        "description": "Sooo fast, but such a pain to handle idempotency because of the <i>at least one</i> semantics."
     },
     {
         "name": "Golang",
         "note": 4,
-        "description": "Really nice language, it is quite new for me but I work with it whenever I can.<span class='invisiblish'>Congratulations Google!</span>"
+        "description": "Really nice language, it is quite new for me but I work with it whenever I can.<span "
+                       "class='invisiblish'>Congratulations Google!</span> "
     },
     {
-        "name": "JAVA & Android",
+        "name": "Nginx",
         "note": 4,
-        "description": "Some experiences in both Java and Android. Java is the language where I learnt all about design patterns."
+        "description": "As good a reverse proxy as a cache engine. Really powerful and lightweight software."
     },
     {
         "name": "Haskell",
         "note": 3,
-        "description": "A new way of thinking for me, but I have a lot of projects at TCD that use Haskell, so I'm learning!"
+        "description": "A new way of thinking for me, but I have a lot of projects at TCD that use Haskell, "
+                       "so I'm learning! "
     },
     {
         "name": "C/C++",
@@ -52,22 +71,32 @@ skills = [
     {
         "name": "Network security",
         "note": 3,
-        "description": "Some tools/techniques I use a lot: <i>nmap</i>, <i>DNS subdomain bruteforce</i>, <i>Recognition based on <a href='https://github.com/darkoperator/dnsrecon/' target='_blank'>dnsrecon</a></i>, <i>Common Sense</i>"
+        "description": "Some tools/techniques I use a lot: <i>nmap</i>, <i>DNS subdomain bruteforce</i>, "
+                       "<i>Recognition based on <a href='https://github.com/darkoperator/dnsrecon/' "
+                       "target='_blank'>dnsrecon</a></i>, <i>Common Sense</i> "
     },
     {
         "name": "PERL",
         "note": 3,
-        "description": "I reviewed a LOT of PERL code during my security internship at OVH, so I don't really have a big experience in writting PERL code but I have quite a nice one in exploiting it."
+        "description": "I reviewed a LOT of PERL code during my security internship at OVH, so I don't really have a "
+                       "big experience in writing PERL code but I have quite a nice one in exploiting it. "
     },
     {
         "name": "PHP",
         "note": 3,
-        "description": "Symphony, Wordpress and scripting. It's a nice language to learn web security.<span class='invisiblish'>troll troll troll</span>"
+        "description": "Symphony, Wordpress and scripting. It's a nice language to learn web security.<span "
+                       "class='invisiblish'>troll troll troll</span> "
+    },
+    {
+        "name": "Zookeeper & Vault",
+        "note": 3,
+        "description": "I used them a lot to retrieve application secrets or variables."
     },
     {
         "name": "SQL",
         "note": 4,
-        "description": "Oracle / MySQL / PostgreSQL / SQLite. I use SQL regularly and I learnt some tricks while playing CTFs. However I never used it on a big database (<1Go)."
+        "description": "Oracle / MySQL / PostgreSQL / SQLite. I use SQL regularly and I learnt some tricks while "
+                       "playing CTFs. However I never used it on a big database (<1Go). "
     },
     {
         "name": "English",
@@ -77,7 +106,7 @@ skills = [
     {
         "name": "ASM",
         "note": 2,
-        "description": "Just the basis, I can read a programm using documentation."
+        "description": "Just the basis, I can read a program using documentation."
     },
     {
         "name": "Spanish",
